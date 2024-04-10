0992f7352d8aa09881d417f99b13db8bcb1e49f1
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-04-10 15:58:09 +0530

    started

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+
+  return (
+    <>
+      A blog app with appwrite
+    </>
+  )
+}
+
+export default App
