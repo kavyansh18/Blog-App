6d55b3dd7e89d17811181ed4e3b93c8cc9f06bc9
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-04-12 23:03:29 +0530

    authentication

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,15 +1,17 @@
-import conf from  '../conf';
-import { Client, Account, ID } from 'appwrite';
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client)
+        this.account = new Account(this.client);
+            
     }
 
     async createAccount({email, password, name}) {
@@ -34,25 +36,26 @@ export class AuthService {
         }
     }
 
-    async  getCurrentUser(){
-        try{
-            return await this.account.get()
-        }catch (err) {
-            throw error;
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
     }
 
-    async logout(){
-        try{
+    async logout() {
+
+        try {
             await this.account.deleteSessions();
-        }catch (error){
-            console.log("Logout failed", error);
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
-const authService = new AuthService()
+const authService = new AuthService();
 
-export default AuthService
-
+export default authService
