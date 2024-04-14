493cd81e1b2a58826082f8cc92ee170674a37d7b
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-04-14 23:29:03 +0530

    added pgs

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "../Post";
+import PostForm from "./post-form/PostForm";
 
 
-export { Header, Footer,Container,Logo, LogoutBtn}
+export { Header, Footer,Container,Logo, LogoutBtn,RTE,Signup,Login,Post,PostForm };
