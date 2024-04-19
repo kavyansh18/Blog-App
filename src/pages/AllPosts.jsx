355df0dd97ec7bb17b8970d3be9957607a61c906
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-04-19 23:57:09 +0530

    fixed create acc issue

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
 
@@ -11,7 +11,7 @@ function AllPosts() {
         }
     })
   return (
-    <div className='w-full py-8'>
+    <div className='w-full py-7'>
         <Container>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import { Login as loginComponent } from '../components'
 function Login() {
   return (
-    <div className='py-8'>
+    <div className='py-7'>
         <loginComponent />
     </div>
   )
